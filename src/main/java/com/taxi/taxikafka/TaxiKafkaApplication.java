f213bf0d945b91c499bf217c4f0fa5e3515cea0b
Le Thanh Son <thanhson@MacBook-Pro-cua-Son.local>
2021-07-21 16:34:18 +0700

    First commit

---
+++ src/main/java/com/taxi/taxikafka/TaxiKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.taxi.taxikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taxi/taxikafka/TaxiKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxi.taxikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
