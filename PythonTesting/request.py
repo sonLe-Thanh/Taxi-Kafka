d3bedc4749796174bdaea5c889410a8673e62959
Le Thanh Son <thanhson@MacBook-Pro-cua-Son.local>
2021-08-09 16:52:26 +0700

    Added Python's GGMap API call

--- PythonTesting/request.py
+++ PythonTesting/request.py
@@ -1,6 +1,56 @@
 import requests
 import time
+import random
 
 start = time.time()
-response=requests.post('https://localhost:8081/kafka/driver/publish')
+my_api_key = "AIzaSyAL0ca3F7rIMYEqOwzaRyERAI0Zpbbgwwo"
+
+class PointCoordinate:
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return str(self.latitude) + ", " + str(self.longitude)
+
+def random_point(num_rand):
+    counter = 0
+    point_list = []
+    max_lat = 10.963984
+    max_long = 106.477055
+
+    min_lat = 10.699614
+    min_long = 106.579939
+    f = open("coordinate.csv", "w+")
+    f.close()
+
+    with open("coordinate.csv", "w") as file:
+        while counter != num_rand:
+            lat = random.uniform(min_lat, max_lat)
+            long = random.uniform(min_long, max_long)
+            new_point = PointCoordinate(lat, long)
+            point_list.append(new_point)
+            file.write(str(new_point)+"\n")
+            counter +=1
+
+    return point_list
+
+num_random = 25
+list_point = random_point(num_random)
+
+src = PointCoordinate(10.838980, 106.647141)
+
+prepare_dest = ""
+for index, point in enumerate(list_point):
+    if index != len(list_point) -1:
+        prepare_dest += str(point) + "|"
+    else:
+        prepare_dest += str(point)
+
+print(prepare_dest)
+
+
+response=requests.post('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+str(src)+
+                       '&destinations='+prepare_dest+'&key='+my_api_key)
+print(response.json())
 print("Execution time: ", time.time()-start)
